29b57583191d39363b1a2b1bf38a81a5ddcbdce6
Azat S <azatklevec@gmail.com>
2015-09-21 00:16:11 +0300

    Earlybird filter

--- index.js
+++ index.js
@@ -146,6 +146,41 @@ module.exports = postcss.plugin('postcss-instagram', function () {
               decl.remove();
             };
 
+          // Filter: Earlybird
+          var filterEarlybird = function(decl) {
+            var origRule = decl.parent,
+              ruleSelectors = origRule.selectors,
+              afterRuleSelectors,
+              imgRuleSelectors,
+              afterRule,
+              earlybirdRule;
+
+              afterRuleSelectors = ruleSelectors.map(function(ruleSelector){
+                    return ruleSelector + ':after';
+              }).join(',\n');
+
+              imgRuleSelectors = ruleSelectors.map(function(ruleSelector){
+                    return ruleSelector + ' img';
+              }).join(',\n');
+
+
+            afterRule = origRule.cloneBefore({
+              selector: afterRuleSelectors
+            }).removeAll();
+
+            earlybirdRule = origRule.cloneBefore({
+              selector: imgRuleSelectors
+            }).removeAll();
+
+            afterRule.append('box-shadow:inset 0 0 3em #222;position:absolute;top:0;right:0;bottom:2px;left:0;z-index:1;content:\'\'');
+
+            origRule.append('position:relative;display:inline-block');
+
+            earlybirdRule.append('filter:url(\'data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feColorMatrix type="matrix" color-interpolation-filters="sRGB" values="0.7572 0.30760000000000004 0.0756 0 0 0.1396 0.8744000000000001 0.06720000000000001 0 0 0.10880000000000001 0.2136 0.6524 0 0 0 0 0 1 0" /><feColorMatrix type="matrix" color-interpolation-filters="sRGB" values="1.6296000000000002 -0.572 -0.0576 0 0 -0.17040000000000002 1.246 -0.0576 0 0 -0.17040000000000002 -0.572 1.7424000000000002 0 0 0 0 0 1 0" /><feComponentTransfer color-interpolation-filters="sRGB"><feFuncR type="linear" slope="1.1" intercept="-0.050000000000000044" /><feFuncG type="linear" slope="1.1" intercept="-0.050000000000000044" /><feFuncB type="linear" slope="1.1" intercept="-0.050000000000000044" /></feComponentTransfer><feComponentTransfer color-interpolation-filters="sRGB"><feFuncR type="linear" slope="0.9" /><feFuncG type="linear" slope="0.9" /><feFuncB type="linear" slope="0.9" /></feComponentTransfer></filter></svg>#filter\'); -webkit-filter:sepia(0.4) saturate(1.8) contrast(1.1) brightness(0.9) hue-rotate(-20deg); filter:sepia(0.4) saturate(1.8) contrast(1.1) brightness(0.9) hue-rotate(-20deg);');
+
+              decl.remove();
+            };
+
       css.walkDecls('filter', function(decl) {
         switch (decl.value) {
           case 'kalvin':
@@ -159,6 +194,9 @@ module.exports = postcss.plugin('postcss-instagram', function () {
             break;
           case 'brannan':
             filterBrannan(decl);
+            break;
+          case 'earlybird':
+            filterEarlybird(decl);
             break;
         }
       });
